0bbcf1cd3672927b84bbf82abe4f9232d4ccb427
02Alexis <m_chael1012@hotmail.com>
2023-11-21 12:05:05 -0500

    what music is playing

--- src/components/CardPlayButton.jsx
+++ src/components/CardPlayButton.jsx
@@ -1,13 +1,27 @@
 import { Pause, Play } from "./Player";
-import { usePlayerStore } from "@/store/playerStore";
+import { usePlayerStore } from "@/store/playerStore"; // estado global
 
 export function CardPlayButton({ id }) {
-  const { currentSong, isPlaying, setIsPlaying, setCurrentSong } =
+  const { currentMusic, isPlaying, setIsPlaying, setCurrentMusic } =
     usePlayerStore((state) => state);
 
+  const handleClick = () => {
+    setCurrentMusic({
+      playlist: {
+        id,
+      },
+    });
+    setIsPlaying(!isPlaying);
+  };
+
+  const setIsPlayingPlayList = isPlaying && currentMusic?.playlist.id == id;
+
   return (
-    <div className="card-play-button rounded-full bg-green-500 p-4">
-      {isPlaying ? <Pause /> : <Play />}
-    </div>
+    <button
+      onClick={handleClick}
+      className="card-play-button rounded-full bg-green-500 p-4"
+    >
+      {setIsPlayingPlayList ? <Pause /> : <Play />}
+    </button>
   );
 }
